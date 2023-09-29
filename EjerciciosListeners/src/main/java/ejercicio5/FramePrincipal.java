72f0417303a97d94608d398aa5afe2d89f536130
Pablo Romero <a21pablorf@iessanclemente.net>
2023-09-29 12:20:06 +0200

    29SEPT

--- EjerciciosListeners/src/main/java/ejercicio5/FramePrincipal.java
+++ EjerciciosListeners/src/main/java/ejercicio5/FramePrincipal.java
@@ -4,9 +4,12 @@
  */
 package ejercicio5;
 
+import java.awt.Color;
 import java.awt.FlowLayout;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.awt.event.MouseAdapter;
+import java.awt.event.MouseEvent;
 import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
@@ -20,6 +23,7 @@ public class FramePrincipal extends JFrame {
     private JLabel etiqueta1;
     private JButton bt1;
     private JButton bt2;
+    private Color colorBase;
 
     public FramePrincipal() {
         super("Ejercicio 4");
@@ -28,6 +32,7 @@ public class FramePrincipal extends JFrame {
         etiqueta1 = new JLabel("Etiqueta 1");
         bt1 = new JButton("BOTON 1");
         bt2 = new JButton("BOTON 2");
+        colorBase = getBackground();
 
         ActionListener al = new ActionListener() {
             @Override
@@ -36,8 +41,24 @@ public class FramePrincipal extends JFrame {
             }
         };
 
+        MouseAdapter l = new MouseAdapter() {
+            @Override
+            public void mouseClicked(MouseEvent e) {
+                Color color = e.getComponent().getBackground();
+
+                //Color color = getBackground();
+                if (color == colorBase) {
+                    e.getComponent().setBackground(Color.yellow);
+                } else {
+                    e.getComponent().setBackground(colorBase);
+                }
+
+            }
+        };
+
         bt1.addActionListener(al);
         bt2.addActionListener(al);
+        getContentPane().addMouseListener(l);
 
         add(etiqueta1);
         add(bt1);
---
+++ Ejercicios_AREAS_TEXTO/Ejercicio1/src/main/java/com/mycompany/ejercicio1/MainFrame.java
@@ -0,0 +1,22 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mycompany.ejercicio1;
+
+import javax.swing.JButton;
+import javax.swing.JLabel;
+import javax.swing.JTextField;
+
+/**
+ *
+ * @author a21pablorf
+ */
+public class MainFrame {
+
+    private JTextField txt1;
+    private JTextField txt2;
+    private JButton btn;
+    private JLabel lb;
+
+}
